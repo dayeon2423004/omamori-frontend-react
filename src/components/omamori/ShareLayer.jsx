48dc734e76631d1d399af7a4ca496ff5b70b5652
dayeon <dayeon2423004@g.yju.ac.kr>
2026-03-02 01:25:23 +0900

    feat: 오마모리 공유 시 프레임 및 요소 조정 & 스타일 추가

--- src/components/omamori/ShareLayer.jsx
+++ src/components/omamori/ShareLayer.jsx
@@ -1,29 +1,32 @@
 export default function ShareLayer({ layer, baseUrl }) {
+    const scale = 0.55; // 캔버스 크기 대비 비율
+
     return (
         <div
             style={{
                 position: "absolute",
-                left: layer.transform.x ?? 0,
-                top: layer.transform.y ?? 0,
-                zIndex: layer.zIndex ?? 1
+                left: (layer.transform.x ?? 0) * scale,
+                top: (layer.transform.y ?? 0) * scale,
+                zIndex: layer.zIndex ?? 1,
+                pointerEvents: "none"
             }}
         >
         {layer.type === "text" && (
-            <p
-                style={{
-                    fontSize: `${layer.props.fontSize}px`,
-                    color: layer.props.color
-                }}
-            >
+            <p style={{
+                fontSize: `${layer.props.fontSize}px`,
+                color: layer.props.color,
+                margin: 0,
+                whiteSpace: "nowrap"
+            }}>
                 {layer.props.content}
             </p>
         )}
 
         {layer.type === "stamp" && (
             <img
-                width={50}
+                width={50 * scale}
                 src={`${baseUrl}${layer.props.url}`}
-                alt=""
+                alt="stamp"
             />
         )}
         </div>
