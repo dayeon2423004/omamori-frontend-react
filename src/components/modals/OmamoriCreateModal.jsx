a6fd252cd85cffbc1786fe0b4adeb48470607b5f
dayeon <dayeon2423004@g.yju.ac.kr>
2026-03-02 01:27:22 +0900

    feat : 오마모리 제목 작성 시 스타일 추가

--- src/components/modals/OmamoriCreateModal.jsx
+++ src/components/modals/OmamoriCreateModal.jsx
@@ -24,17 +24,48 @@ export default function OmamoriCreateModal({ onClose }) {
     }
 
     return (
-        <div className="modal">
-            <form onSubmit={handleSubmit}>
+        <div className="modal" style={{
+            background: "#fff",
+            padding: "40px",
+            borderRadius: "10px",
+            width: "420px",
+            boxShadow: "0 20px 40px rgba(0,0,0,0.1)"
+        }}>
+            <form onSubmit={handleSubmit}
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "20px"
+                }}>
                 <input
                     type="text"
                     name="title"
-                    placeholder="오마모리 제목"
+                    placeholder="오마모리 제목을 입력하세요."
                     value={userTitle}
                     onChange={(e) => {setUserTitle(e.target.value)}}
+                    style={{
+                        padding: "14px 16px",
+                        borderRadius: "10px",
+                        border: "1px solid #ddd",
+                        fontSize: "14px",
+                        outline: "none"
+                    }}
                 />
             
-                <button type="submit">제목 생성</button>
+                <button 
+                    type="submit"
+                    style={{
+                        padding: "12px",
+                        borderRadius: "5px",
+                        border: "none",
+                        background: "#7b96ff",
+                        color: "white",
+                        fontWeight: "600",
+                        cursor: "pointer"
+                    }}
+                >
+                    제목 생성
+                </button>
             </form>
         </div>
     )
