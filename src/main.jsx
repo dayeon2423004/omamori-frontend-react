d00c5cbc7b3946ff51e3f5788e78bf4635c0d55e
dayeon <dayeon2423004@g.yju.ac.kr>
2026-03-03 01:19:22 +0900

    feat: HelmetProvider 추가

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,9 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ModalProvider } from "./context/ModalContext";
-import { AuthProvider } from './context/AuthContext.jsx'
-import { FortuneProvider } from './context/FortuneContext.jsx'
+import { AuthProvider } from './context/AuthContext.jsx';
+import { FortuneProvider } from './context/FortuneContext.jsx';
+import { HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -13,7 +14,9 @@ createRoot(document.getElementById('root')).render(
       <ModalProvider>
         <AuthProvider>
           <FortuneProvider>
-            <App />
+            <HelmetProvider>
+              <App />
+            </HelmetProvider>
           </FortuneProvider>
         </AuthProvider>
       </ModalProvider>
