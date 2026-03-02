b67b101bccfb4456085f01f66ca99ee0832f05af
dayeon <dayeon2423004@g.yju.ac.kr>
2026-03-03 03:35:33 +0900

    feat: console 추가

--- src/pages/SharePage.jsx
+++ src/pages/SharePage.jsx
@@ -60,6 +60,7 @@ export default function SharePage() {
             )}
             
             <div className="contentWrapper">
+                {console.log("되나")}
                 {/* 상단 텍스트 영역 */}
                 <motion.div 
                     initial={{ opacity: 0, y: -20 }} 
